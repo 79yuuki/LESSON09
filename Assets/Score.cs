bfcaa0a88036d96adffb62f2010343b581db90d6
79yuuki <yuuki.79@gmail.com>
2013-12-08 18:55:21 +0900

    first commit

---
+++ Assets/Score.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	//  スコア
+	private int score;
+	
+	//  スコアを初期化する
+	void InitScore() {
+		this.score = 0;
+	}
+
+	//  スコアを加算する
+	void AddScore(int score) {
+		this.score += score;
+	}
+	
+	void Update () {
+		// GUI Textを書き換える
+		guiText.text = "Score:" + this.score;	
+	}
+}
